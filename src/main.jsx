3a5fef8d2f4ba11588578786e81f5e39f4a6e452
akash.ssingh.aakkaa@gmail.com <akash.ssingh.aakkaa@gmail.com>
2024-05-01 13:54:21 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Movies from "./pages/Movies.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
